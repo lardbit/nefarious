c2721c2f3566093d65079b878c9c9d67e822ff21
lardbit <dr@strangelove.com>
2019-06-03 20:39:50 -0400

    fix transmission connection issues

--- generate-arm7-docker-compose.sh
+++ generate-arm7-docker-compose.sh
@@ -4,7 +4,7 @@
 
 cp docker-compose.yml docker-compose.arm.yml
 sed -i "s/image:\ lardbit\/nefarious/image:\ lardbit\/nefarious:armv7/g" docker-compose.arm.yml
-sed -i "s/image:\ linuxserver\/transmission/image:\ linuxserver\/transmission:arm32v7-latest/g" docker-compose.arm.yml
+sed -i "s/image:\ linuxserver\/transmission/image:\ linuxserver\/transmission:armhf/g" docker-compose.arm.yml
 sed -i "s/image:\ linuxserver\/jackett/image:\ linuxserver\/jackett:arm32v7-latest/g" docker-compose.arm.yml
 sed -i "s/image:\ redis/image:\ arm32v7\/redis/g" docker-compose.arm.yml
 sed -i "s/image:\ v2tec\/watchtower/image:\ v2tec\/watchtower:armhf-latest/g" docker-compose.arm.yml
