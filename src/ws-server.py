6a67852420db08892aff68fbfbc9f78de4f1afba
lardbit <dr@strangelove.com>
2020-01-31 11:30:51 -0600

    python 3.8, websockets server (WIP)

--- src/ws-server.py
+++ src/ws-server.py
@@ -1,22 +1,14 @@
 import asyncio
-import json
 import logging
 import websockets
 
 logging.basicConfig()
 
-STATE = {}
-
 USERS = set()
 
 
-def state_event():
-    return json.dumps(STATE)
-
-
-async def notify_state():
+async def notify_state(message):
     if USERS:  # asyncio.wait doesn't accept an empty list
-        message = state_event()
         await asyncio.wait([user.send(message) for user in USERS])
 
 
@@ -28,20 +20,17 @@ async def unregister(websocket):
     USERS.remove(websocket)
 
 
-async def counter(websocket, path):
+async def handle(websocket, path):
     await register(websocket)
     try:
-        await websocket.send(state_event())
         async for message in websocket:
-            data = json.loads(message)
-            logging.warning(data)
-            STATE.update(data)
-            await notify_state()
+            logging.warning(message)
+            await notify_state(message)
     finally:
         await unregister(websocket)
 
 
-start_server = websockets.serve(counter, "localhost", 6789)
+start_server = websockets.serve(handle, "localhost", 6789)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
