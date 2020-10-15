07acd3ad4116be991da49b390c0884499146db46
lardbit <dr@strangelove.com>
2020-10-15 16:57:04 -0500

    include config path as env variable

--- src/nefarious/settings.py
+++ src/nefarious/settings.py
@@ -174,8 +174,10 @@ INTERNAL_DOWNLOAD_PATH = os.environ.get('INTERNAL_DOWNLOAD_PATH', '/tmp')
 # nefarious (celery) will actually use the INTERNAL_DOWNLOAD_PATH (container specific path) to scan for imported media
 HOST_DOWNLOAD_PATH = os.environ.get('HOST_DOWNLOAD_PATH', INTERNAL_DOWNLOAD_PATH if DEBUG else None)
 
-# log to shared /nefarious-db path when using default container configuration, otherwise fallback to /tmp
-log_path = '/nefarious-db' if os.path.exists('/nefarious-db') else '/tmp'
+CONFIG_PATH = os.environ.get('CONFIG_PATH', '/nefarious-db')
+
+# log to shared config path when using default container configuration, otherwise fallback to /tmp
+log_path = CONFIG_PATH if os.path.exists(CONFIG_PATH) else '/tmp'
 NEFARIOUS_LOG_FILE_FOREGROUND = os.path.join(log_path, '.nefarious-foreground.log')
 NEFARIOUS_LOG_FILE_BACKGROUND = os.path.join(log_path, '.nefarious-background.log')
 
