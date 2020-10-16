b35ee7f7635fb27bb892bf6bf4a8235d23c7cc57
lardbit <dr@strangelove.com>
2020-10-16 18:27:01 -0500

    conditionally post to webhook

--- src/nefarious/webhook.py
+++ src/nefarious/webhook.py
@@ -5,9 +5,10 @@ from nefarious.utils import logger_background
 
 def send_message(message: str):
     nefarious_settings = NefariousSettings.get()
-    response = requests.post(nefarious_settings.webhook_url, json={nefarious_settings.webhook_key: message}, timeout=5)
-    try:
-        response.raise_for_status()
-    except Exception as e:
-        logger_background.warning('webhook error for url {} and data key "{}"'.format(nefarious_settings.webhook_url, nefarious_settings.webhook_key))
-        logger_background.exception(e)
+    if nefarious_settings.webhook_url and nefarious_settings.webhook_key:
+        response = requests.post(nefarious_settings.webhook_url, json={nefarious_settings.webhook_key: message}, timeout=5)
+        try:
+            response.raise_for_status()
+        except Exception as e:
+            logger_background.warning('webhook error for url {} and data key "{}"'.format(nefarious_settings.webhook_url, nefarious_settings.webhook_key))
+            logger_background.exception(e)
