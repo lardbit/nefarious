2cad31427f5f3fb1c20a5103b326b33eb4e69c49
lardbit <lardbit@gmail.com>
2021-01-14 06:04:11 -0600

    Revert "run nefarious as a non-privileged user while still binding container's port to 80 for backwards compatibility, update the docker-compose.transmission-vpn.yml to support the UID/GID environment variables"
    This reverts commit 45cdc455a79a6e54dde5a0ad39f838e40d774e35.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,4 +7,4 @@
 /env/bin/python manage.py nefarious-init ${NEFARIOUS_USER-admin} ${NEFARIOUS_EMAIL-admin@localhost} ${NEFARIOUS_PASS-admin}
 
 # run app
-authbind /env/bin/uvicorn --workers 2 --host 0.0.0.0 --port 80 nefarious.asgi:application
+/env/bin/uvicorn --workers 2 --host 0.0.0.0 --port 80 nefarious.asgi:application
