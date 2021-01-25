ad32b85c82aecac7e3687ca7918b79a08dfafac5
dr strangelove <dr@strangelove.com>
2021-01-25 09:48:59 -0600

    chown app directory too for celery beat schedule

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ CONFIG_PATH=/nefarious-db/
 # set config path permissions to be writable by the non-root user
 if [ -e "${CONFIG_PATH}" ]
   then
-    chown -R $RUN_AS_USER:$RUN_AS_USER "${CONFIG_PATH}"
+    chown -R $RUN_AS_USER:$RUN_AS_USER "${CONFIG_PATH}" .
   else
     echo config path ${CONFIG_PATH} does not exist so cannot set permissions
 fi
