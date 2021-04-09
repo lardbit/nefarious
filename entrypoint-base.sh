27fb0b62db14cc3b47b1375d13dc900f55eb48d3
lardbit <lardbit@gmail.com>
2021-04-09 15:11:13 -0500

    fix entrypoints to run as user

--- entrypoint-base.sh
+++ entrypoint-base.sh
@@ -1,11 +1,9 @@
-#!/bin/sh
-
 # nefarious user and specified uid
 RUN_AS_USER=nefarious
 RUN_AS_UID=${HOST_DOWNLOAD_UID-1000}
 
 # create if it does not already exist
-id -u ${RUN_AS_UID} &>/dev/null || useradd -u ${RUN_AS_UID} ${RUN_AS_USER}
+id -u ${RUN_AS_UID} 2>/dev/null || useradd -u ${RUN_AS_UID} ${RUN_AS_USER}
 
-# set file permission on configuration directory
-chown -R ${RUN_AS_UID} . /nefarious-db
+# set folder permissions
+chown -R ${RUN_AS_UID} /app /nefarious-db
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,15 @@
 . $(dirname $0)/entrypoint-base.sh
 
 # build django database/tables
-/env/bin/python manage.py migrate
+su $(id -un ${RUN_AS_UID}) -c "/env/bin/python manage.py migrate"
 
 # create default super user
-/env/bin/python manage.py nefarious-init "${NEFARIOUS_USER-admin}" "${NEFARIOUS_EMAIL-admin@localhost}" "${NEFARIOUS_PASS-admin}"
+su $(id -un ${RUN_AS_UID}) -c "/env/bin/python manage.py nefarious-init ${NEFARIOUS_USER-admin} ${NEFARIOUS_EMAIL-admin@localhost} ${NEFARIOUS_PASS-admin}"
 
-# run app as requested user
+# allow user to bind to port 80
+touch /etc/authbind/byport/80
+chmod 500 /etc/authbind/byport/80
+chown ${RUN_AS_UID} /etc/authbind/byport/80
+
+# run as requested user
 su $(id -un ${RUN_AS_UID}) -c "authbind /env/bin/uvicorn --workers 2 --host 0.0.0.0 --port 80 nefarious.asgi:application"
