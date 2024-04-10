ee2c4fdd1a23358a4aa29459ba5d8adca7ff9ba6
lardbit <lardbit@gmail.com>
2024-04-10 11:56:08 -0500

    run celery scheduler (beat) separately so concurrency doesn't create duplicate tasks, decrease logging sizes

--- entrypoint-celery.sh
+++ entrypoint-celery.sh
@@ -14,7 +14,8 @@ if [ ! -z $CELERY_BEAT_SEPARATELY ]; then
   else
     su $(id -un ${RUN_AS_UID}) -c "/env/bin/celery -A nefarious worker --concurrency ${NUM_CELERY_WORKERS:-0} --loglevel=INFO"
   fi
-# run celery worker & scheduler at the same time, with single concurrency
+# run celery worker & scheduler at the same time
+# NOTE: this isn't advised since the scheduler could be potentially running multiple/simultaneous times creating duplicate tasks
 else
-  su $(id -un ${RUN_AS_UID}) -c "/env/bin/celery -A nefarious worker --concurrency 1 --beat --loglevel=INFO"
+  su $(id -un ${RUN_AS_UID}) -c "/env/bin/celery -A nefarious worker --concurrency ${NUM_CELERY_WORKERS:-0} --beat --loglevel=INFO"
 fi
