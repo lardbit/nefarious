760af98289ec583ccf1b69d797b7348d6f725d51
lardbit <lardbit@gmail.com>
2025-01-20 10:06:16 -0600

    update video detection to skip forward frames and dynamically slice 100 total frames to analyze

--- src/nefarious/video_detection.py
+++ src/nefarious/video_detection.py
@@ -9,20 +9,17 @@ from nefarious.utils import logger_background
 
 
 class VideoDetect:
-    CAPTURE_FRAME_SECONDS = 5  # capture frames every x seconds
     MIN_VIDEO_SIMILARITY_STD = .05  # "real" videos should be > .15 (from 0-1)
-    MAX_VIDEO_DURATION_DIFFERENCE_RATIO = .2  # the duration should be within x% of the actual
+    NUM_CAPTURE_FRAMES = 100
 
     video_path: str
     video_capture = None
     video_similarity_std: float  # from 0-1
     frame_count: int
     frame_rate: int
-    duration: float
-    read_interval: int
+    frame_interval: int
 
     def __init__(self, video_path: str):
-        logger_background.debug(f'VideoDetect: Initializing video capture {video_path}')
         # video capture
         self.video_path = video_path
         self.video_capture = cv2.VideoCapture(self.video_path)
@@ -30,8 +27,9 @@ class VideoDetect:
         # get video information
         self.frame_count = int(self.video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
         self.frame_rate = int(self.video_capture.get(cv2.CAP_PROP_FPS))
-        self.read_interval = int((self.frame_rate * self.CAPTURE_FRAME_SECONDS) - 1)
-        self.duration = self.frame_count / self.frame_rate
+        self.frame_interval = int(self.frame_count * (1 / self.NUM_CAPTURE_FRAMES))  # capture NUM_CAPTURE_FRAMES worth of frames
+
+        logger_background.info(f'VideoDetect: Initializing video capture {video_path}: frame_count = {self.frame_count}, frame_rate = {self.frame_rate}, frame_interval = {self.frame_interval}')
 
     @classmethod
     def has_valid_video_in_path(cls, path: str):
@@ -73,9 +71,6 @@ class VideoDetect:
         logger_background.warning(f'[VIDEO_DETECTION] no valid files found in {path}')
         return False
 
-    def is_correct_length(self, expected_duration: float):
-        return abs(self.duration - expected_duration) / expected_duration < self.MAX_VIDEO_DURATION_DIFFERENCE_RATIO
-
     def is_too_similar(self):
         logger_background.info(f'[VIDEO_DETECTION] video_similarity_std {self.video_similarity_std=}, {self.MIN_VIDEO_SIMILARITY_STD=}, {self.video_path=}')
         return self.video_similarity_std <= self.MIN_VIDEO_SIMILARITY_STD
@@ -85,11 +80,14 @@ class VideoDetect:
         histograms = []
 
         # generate histograms for every captured frame
-        for i in range(self.frame_count):
-            if i == 0 or i % self.read_interval == 0:
-                success, image = self.video_capture.read()
-                gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                histograms.append(cv2.calcHist([gray_image], [0], None, [256], [0, 256]))
+        for frame in range(0, self.frame_count, self.frame_interval):
+            # read specific frame
+            self.video_capture.set(cv2.CAP_PROP_POS_FRAMES, frame)
+            success, image = self.video_capture.read()
+            if not success:
+                continue
+            gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            histograms.append(cv2.calcHist([gray_image], [0], None, [256], [0, 256]))
 
         # randomize histograms and compare frame by frame
         random.shuffle(histograms)
