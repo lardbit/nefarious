f358a688a485564520f7b6e6eef615dd6b576b13
lardbit <lardbit@gmail.com>
2025-03-01 10:13:37 -0600

    don't use a pid file with celery scheduler

--- entrypoint-celery.sh
+++ entrypoint-celery.sh
@@ -17,5 +17,5 @@ if [ ! -z $CELERY_BEAT_SEPARATELY ]; then
 # run celery worker & scheduler at the same time
 # NOTE: this isn't advised since the scheduler could be potentially running multiple/simultaneous times creating duplicate tasks
 else
-  su $(id -un ${RUN_AS_UID}) -c "/env/bin/celery -A nefarious worker --concurrency ${NUM_CELERY_WORKERS:-0} --beat --loglevel=INFO"
+  su $(id -un ${RUN_AS_UID}) -c "/env/bin/celery -A nefarious worker --concurrency ${NUM_CELERY_WORKERS:-0} --beat --pidfile= --loglevel=INFO"
 fi
